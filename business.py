734c675bcd756e53bff520177024a9ef5f0e8cc7
theo <matchoc64600@gmail.com>
2026-09-22 10:31:45 +0200

    Iss project api done

---
+++ business.py
@@ -0,0 +1,50 @@
+import requests
+from datetime import datetime
+
+
+def call_api(methode, url):
+    try:
+        response = requests.request(methode, url, timeout=10)
+        return response.json()
+    except requests.RequestException as e:
+        print("API call Error", e)
+        return "Call api Error"
+
+
+def format_timestamp(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+def iss_position():
+    data = call_api("GET", "http://api.open-notify.org/iss-now.json")
+
+    if data:
+        formatted_data = {
+            "latitude": data["iss_position"]["latitude"],
+            "longitude": data["iss_position"]["longitude"],
+            "timestamp": format_timestamp(data["timestamp"]),
+        }
+    else:
+        return "No data"
+    return formatted_data
+
+def astronauts_position():
+    data = call_api("GET", "http://api.open-notify.org/astros.json")
+
+    if data:
+        astronauts = []
+        astronauts_length = len(data["people"])
+
+        astronauts.append(
+            f"nombre : {astronauts_length}")
+
+        for i in data["people"]:
+            astronauts.append({
+                "name": i["name"]
+            })
+
+        return astronauts
+
+    else:
+        return "No data"
+
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World")
+from business import iss_position,astronauts_position
+
+continuer = True
+
+while continuer:
+    print()
+    print("Menu ISS")
+    print(50 * "=")
+    print("1 - Afficher la position de l'ISS")
+    print("2 - Afficher les occupants de l'ISS")
+    print("0 - Quitter")
+    print()
+
+    choix = input("Votre choix: ")
+
+    if choix == "1":
+        print(iss_position())
+
+    elif choix == "2":
+        print(astronauts_position())
+
+    elif choix == "0":
+        print("Au revoir !")
+        continuer = False
+
+    else:
+        print("Choix invalide. Veuillez choisir une option entre 0 et 3.")
